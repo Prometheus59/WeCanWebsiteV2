fc7bb4d994dd8c13b39e7016cc4dd6e21d7ec996
Ryan Karumanchery <ryankarumanchery@gmail.com>
2018-06-14 10:15:13 -0400

    Added gmaps and business info to contact page

---
+++ src/BizInfo.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default class BizInfo extends React.Component {
+  render () {
+    return (
+      <div className='contact-info'>
+        <div id='address'>
+          Address 1
+          <br /> Address 2
+          <br /> Fax Address
+          <br /> (000) 000 - 000
+          <br /> email@example.ca
+          <br />
+        </div>
+        <div id='hours'>
+          Hours of Operation
+          <br /> Monday-Friday
+          <br /> 8:30am - 4:30pm
+        </div>
+      </div>
+    )
+  }
+}
--- src/Contact.js
+++ src/Contact.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import Form from "./Form";
-import './css/About.css';
-
+import React from 'react'
+import Form from './Form'
+import BizInfo from './BizInfo'
+import Gmaps from './Gmaps'
 
 export default class Contact extends React.Component {
   render () {
     return (
-        <div>
+      <div className='formContainer'>
+        <div className='companyInfo'>
           <Form />
+          <BizInfo />
         </div>
+        <Gmaps />
+      </div>
     )
   }
 }
