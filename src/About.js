3070ae130e6e285c1798b434658db5da7d31f384
Ryan Karumanchery <ryankarumanchery@gmail.com>
2018-06-21 11:49:03 -0400

    Fixed position of text to image in

--- src/About.js
+++ src/About.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import pagesrc from './images/MainPage.jpg';
-import './css/About.css';
-import PaperSheet from './Paper';
+import React from 'react'
+import pagesrc from './images/MainPage.jpg'
+import './css/About.css'
+import PaperSheet from './Paper'
 
 function Page () {
-  return <img src={pagesrc} alt='mainImage'/>
+  return <img src={pagesrc} alt='mainImage' />
 }
 
 export default class About extends React.Component {
   render () {
     return (
-      <div id="aboutUs">
-        <Page />
-        <div className='centered'>WeCan Insurance</div>
-      <br/>
+      <div>
+        <div id='aboutUs'>
+          <Page />
+          <div className='centered'>WeCan Insurance</div>
+        </div>
+        <br />
         <div className='paper'>
-        <PaperSheet title='Title' message='message' />
+          <PaperSheet title='Title' message='message' />
         </div>
       </div>
     )
