b0d29a024bf613b6497d6c8a54b49e7cb4406a43
Ryan Karumanchery <ryankarumanchery@gmail.com>
2018-06-26 13:39:45 -0400

    Navbar is now sticky

--- src/Common/Tabs.js
+++ src/Common/Tabs.js
@@ -6,7 +6,8 @@ import About from '../About/About'
 import Services from '../Services/Services'
 import Contact from '../Contact/Contact'
 import Header from '../Common/Header'
-import { NavLink, BrowserRouter, Route, Switch } from 'react-router-dom';
+import { NavLink, BrowserRouter, Route, Switch } from 'react-router-dom'
+import Sticky from 'react-sticky-el'
 
 class TabMenu extends React.Component {
   state = {
@@ -24,20 +25,31 @@ class TabMenu extends React.Component {
       <BrowserRouter>
         <div>
           <Header />
-          <AppBar position='static'>
-            <Tabs value={value} onChange={this.handleChange}>
-              <Tab label='About' component={NavLink} to='/about'/>
-              <Tab label='Services' component={NavLink} to='/services'/>
-              <Tab label='Sound Insurance' disabled component={NavLink} to='/soundInsurance' />
-              <Tab label='Contact' component={NavLink} to='/contact'/>
-            </Tabs>
-          </AppBar>
+          <Sticky>
+            <AppBar position='static' id='navBar'>
+              <Tabs value={value} onChange={this.handleChange}>
+                <Tab label='About' component={NavLink} to='/about' />
+                <Tab label='Services' component={NavLink} to='/services' />
+                <Tab
+                  label='Sound Insurance'
+                  disabled
+                  component={NavLink}
+                  to='/soundInsurance'
+                />
+                <Tab label='Contact' component={NavLink} to='/contact' />
+              </Tabs>
+            </AppBar>
+          </Sticky>
           <div className='content'>
             <Switch>
               <Route exact path='/' component={About} />
               <Route path='/services' component={Services} />
               <Route path='/about' component={About} />
-              <Route path='/soundInsurance' component={() => window.location.href = 'http://www.soundinsurance.ca'} />
+              <Route
+                path='/soundInsurance'
+                component={() =>
+                  (window.location.href = 'http://www.soundinsurance.ca')}
+              />
               <Route path='/contact' component={Contact} />
             </Switch>
           </div>
