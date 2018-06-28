346557ac527edc13044c3ebea13d7b45a49bbc4b
Ryan Karumanchery <ryankarumanchery@gmail.com>
2018-06-28 10:25:28 -0400

    Added translate toggle to header

--- src/Common/Header.js
+++ src/Common/Header.js
@@ -3,6 +3,7 @@ import '../css/Header.css'
 import icon from '../images/newblue.png'
 import Button from '@material-ui/core/Button';
 import { NavLink } from 'react-router-dom';
+import Translate from './Translation';
 
 const logo = icon;
 
@@ -12,7 +13,9 @@ export default class Header extends React.Component {
       <div className='Header'>
         <img className='logo' src={logo} alt='WeCan Logo' />
         <div className='title'>WeCan Insurance</div>
+
         <div className='btn'>
+        <Translate/>
         <Button variant="contained" color="primary" className="contact-btn" component={NavLink} to='/contact'>
         Contact Us
       </Button>
---
+++ src/Common/Translation.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default class Translate extends React.Component {
+  render () {
+    return <div id='google_translate_element' />
+  }
+}
