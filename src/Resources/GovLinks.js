95e706cd36f89bb0369ba35a4881ba37a954a542
Ryan Karumanchery <ryankarumanchery@gmail.com>
2018-07-06 09:50:51 -0400

    Added ability to toggle information

--- src/Resources/GovLinks.js
+++ src/Resources/GovLinks.js
@@ -1,19 +1,38 @@
-import React from 'react';
+import React from 'react'
 
 export default class GovLink extends React.Component {
-    render() {
-        return (
-             <div>
-              <h3>Government Links</h3>
-              <div id='hidden1'>
-                <ul>
-                  <li>Testing</li>
-                  <li>Test</li>
-                  <li>More Testing</li>
-                  <li>other stuffs</li>
-                </ul>
-              </div>
-            </div>
-        )
-    }
+  constructor () {
+    super()
+    this.state = { hidden: false }
+
+    this.handleChange = this.handleChange.bind(this)
+
+    this.toggleInstance = (
+      <div onClick={this.handleChange}>
+        <h3>Government Links</h3>
+      </div>
+    )
+  }
+
+  handleChange (event) {
+    this.setState({ hidden: !this.state.hidden })
+  }
+
+  render () {
+    const style = this.state.hidden ? { display: 'none' } : {}
+
+    return (
+      <div>
+        {this.toggleInstance}
+        <div id='hidden1' style={style}>
+          <ul>
+            <li>Testing</li>
+            <li>Test</li>
+            <li>More Testing</li>
+            <li>look at this, even more testing</li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
 }
