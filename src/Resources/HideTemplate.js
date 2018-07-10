574d2abd74bd60b72261ccb413731d6a57d9eddd
Ryan Karumanchery <ryankarumanchery@gmail.com>
2018-07-10 09:10:47 -0400

    Added transition opening to resource page

--- src/Resources/HideTemplate.js
+++ src/Resources/HideTemplate.js
@@ -3,30 +3,33 @@ import React from 'react'
 export default class HideTemplate extends React.Component {
   constructor (props) {
     super(props)
-    this.state = { hidden: true }
+    this.state = { visible: false } // Change to visible
 
     this.handleChange = this.handleChange.bind(this)
 
     this.toggleInstance = (
       <div onClick={this.handleChange}>
-        <h3>{this.props.title}</h3>
+        <h3 className='titles'>{this.props.title}</h3>
       </div>
     )
   }
 
   handleChange (event) {
-    this.setState({ hidden: !this.state.hidden })
+    this.setState({ visible: !this.state.visible }) //Change this to visible too
   }
 
   render () {
-    const style = this.state.hidden ? { display: 'none' } : {}
+    const style = this.state.visible ? { visibility: 'visible',
+       transition: 'visibility 0s, opacity 0.5s linear', opacity:1 }: 
+       {visibility: 'hidden', opacity: 0,
+       height: 0}
 
     return (
       <div>
         {this.toggleInstance}
         <div style={style}>
         {/*Children prop passes children elements directly into their output --> see GovLinks.js for more detail*/}
-          {this.props.children}
+          <div className='links'>{this.props.children}</div>
         </div>
       </div>
     )
--- src/Resources/Resources.js
+++ src/Resources/Resources.js
@@ -2,6 +2,7 @@ import React from 'react'
 import GovLink from './GovLinks'
 import Dresources from './Dresources'
 import FAQ from './FAQ';
+import './Resource.css';
 
 export default class Resources extends React.Component {
   render () {
