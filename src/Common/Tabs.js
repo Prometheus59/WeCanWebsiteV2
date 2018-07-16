eafebc5596f65210953199b3d7cb26805ed31abf
Ryan Karumanchery <ryankarumanchery@gmail.com>
2018-07-16 11:07:34 -0400

    Reformatted code

--- src/Common/Tabs.js
+++ src/Common/Tabs.js
@@ -1,14 +1,15 @@
-import React from 'react'
-import AppBar from '@material-ui/core/AppBar'
-import Tabs from '@material-ui/core/Tabs'
-import Tab from '@material-ui/core/Tab'
-import About from '../About/About'
-import Services from '../Services/Services'
-import Contact from '../Contact/Contact'
-import Header from '../Common/Header'
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import About from '../About/About';
+import Services from '../Services/Services';
+import Contact from '../Contact/Contact';
+import Header from '../Common/Header';
 import Resources from '../Resources/Resources';
-import { NavLink, Route, Switch } from 'react-router-dom'
-import Sticky from 'react-sticky-el'
+import { NavLink, Route, Switch } from 'react-router-dom';
+import Sticky from 'react-sticky-el';
+import '../css/Navbar.css';
 
 class TabMenu extends React.Component {
   state = {
--- src/Services/Services.js
+++ src/Services/Services.js
@@ -6,20 +6,20 @@ import '../css/Services.css';
 export default class Services extends React.Component {
     render() {
 
-      
+
         return (
             // Make this a carousel selector
-             <div>
-                 <h1>Services Available</h1>
-                 <SimpleSlider />
-                 {/*TODO: Add subsections here for different insurance*/}
-                 <div className='detailedInsurance'>
-                 <div id='auto'>Car Insurance</div>
-                 <div id='home'>home Insurance</div>
-                 <div id='commercial'>comm Insurance</div>
-                 <div id='travel'>travel Insurance</div>
-                 </div>
-             </div>
+            <div>
+                <h1>Services Available</h1>
+                <SimpleSlider />
+                {/*TODO: Add subsections here for different insurance*/}
+                <div className='detailedInsurance'>
+                    <div id='auto'>Car Insurance</div>
+                    <div id='home'>home Insurance</div>
+                    <div id='commercial'>comm Insurance</div>
+                    <div id='travel'>travel Insurance</div>
+                </div>
+            </div>
         )
     }
 }
