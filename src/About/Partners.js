0df8ced66965cb5f7c78ea935af990bae9f14482
Ryan Karumanchery <ryankarumanchery@gmail.com>
2018-07-24 12:23:34 -0400

    Indentation changes, words added

--- src/About/Partners.js
+++ src/About/Partners.js
@@ -4,8 +4,13 @@ import React from 'react';
 export default class Partners extends React.Component {
     render(){
         return(
-            <div>
-                List of things
+            <div id='partners'>
+                <h1>Insurance Partners</h1>
+                <ul>
+                    <li>Partner 1</li>
+                    <li>Partner 2</li>
+                    <li>Partner 3</li>
+                </ul>
             </div>
         )
     }
