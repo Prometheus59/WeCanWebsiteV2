32e50d18711c0f2e5e8a34de03b5c562dcca3f4a
Ryan Karumanchery <ryankarumanchery@gmail.com>
2018-08-03 09:46:32 -0400

    Code reformatted

--- src/Services/services.jsx
+++ src/Services/services.jsx
@@ -1,30 +1,28 @@
-import React from 'react';
-import SimpleSlider from './carousel';
-import './Services.css';
-import Auto from './auto';
-import Home from './home';
-import Commercial from './commercial';
-import Travel from './travel';
-import MobileSimpleSlider from '../Mobile/MobileCarousel';
-
+import React from "react";
+import SimpleSlider from "./carousel";
+import "./Services.css";
+import Auto from "./auto";
+import Home from "./home";
+import Commercial from "./commercial";
+import Travel from "./travel";
+import MobileSimpleSlider from "../Mobile/MobileCarousel";
 
 export default class Services extends React.Component {
-    render() {
-
-        return (
-            // Make this a carousel selector
-            <div>
-                <div id='ellip'>
-                    <SimpleSlider />
-                    <MobileSimpleSlider />
-                </div>
-                <div className='detailedInsurance'>
-                    <Auto />
-                    <Home />
-                    <Commercial />
-                    <Travel />
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      // Make this a carousel selector
+      <div>
+        <div id="ellip">
+          <SimpleSlider />
+          <MobileSimpleSlider />
+        </div>
+        <div className="detailedInsurance">
+          <Auto />
+          <Home />
+          <Commercial />
+          <Travel />
+        </div>
+      </div>
+    );
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './css/index.css'
-import 'typeface-roboto'
-import TabMenu from './Common/Tabs'
-import Footer from './Common/Footer'
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./css/index.css";
+import "typeface-roboto";
+import TabMenu from "./Common/Tabs";
+import Footer from "./Common/Footer";
+import { BrowserRouter } from "react-router-dom";
 
-require('typeface-roboto')
+require("typeface-roboto");
 
 class App extends React.Component {
-  render () {
+  render() {
     return (
       <BrowserRouter>
         <div>
@@ -17,9 +17,9 @@ class App extends React.Component {
           <Footer />
         </div>
       </BrowserRouter>
-    )
+    );
   }
 }
 
 // ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root"));
