ef226f2c534c3d13fa05ad6110c428bf7106d21e
Ryan Karumanchery <ryankarumanchery@gmail.com>
2018-08-13 12:05:22 -0400

    Changed partners page font

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./css/index.css";
 import "typeface-roboto";
+import "typeface-open-sans";
 import TabMenu from "./Common/Tabs";
 import Footer from "./Common/Footer";
 import { BrowserRouter } from "react-router-dom";
 
 require("typeface-roboto");
+require("typeface-open-sans");
 
 class App extends React.Component {
   render() {
