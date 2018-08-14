1bac6813768e07f0d55427314f5e8e34b9f0c0b9
Ryan Karumanchery <ryankarumanchery@gmail.com>
2018-08-14 15:22:14 -0400

    Fixed footer links for services
    Added back id tags to each of the service sections for hash links

--- src/Services/services.jsx
+++ src/Services/services.jsx
@@ -28,7 +28,7 @@ export default class Services extends React.Component {
       "Lost/Delayed/Stolen baggage",
       "If travel insurance is a requirement in the country",
       "Emergency cancellations"
-    ]
+    ];
     return (
       // Make this a carousel selector
       <div>
@@ -37,50 +37,58 @@ export default class Services extends React.Component {
           <MobileSimpleSlider />
         </div>
         <div className="detailedInsurance">
-          <ServiceTemplate
-            title="Auto Insurance"
-            message="Vehicle insurance in Ontario is not just a
+          <div id="auto">
+            <ServiceTemplate
+              title="Auto Insurance"
+              message="Vehicle insurance in Ontario is not just a
                      preventative measure, it's a requirement. If you are caught driving without valid vehicle insurance,
                     you may face fines from $5,000 - $50,000 and risk having your driver's license 
                     suspended and vehicle impounded."
-            message2="Here at WeCan Insurance, we compare quotes with our numerous insurance partners to offer you 
+              message2="Here at WeCan Insurance, we compare quotes with our numerous insurance partners to offer you 
             personalized service with the best rates and coverage available for your vehicle."
-            listIntro="We offer insurance for many vehicles including:"
-            items={autoList}
-          />
-          <ServiceTemplate
-            title="Home Insurance"
-            message="A house is often the largest financial investment someone will make in their entire life,
+              listIntro="We offer insurance for many vehicles including:"
+              items={autoList}
+            />
+          </div>
+          <div id="home">
+            <ServiceTemplate
+              title="Home Insurance"
+              message="A house is often the largest financial investment someone will make in their entire life,
           so it makes sense to protect that investment from potential disasters. Water damage, theft,
           fire, crashes from aircraft or automobiles, and explosions are just a few possible reasons
           to consider purchasing house insurance."
-            message2="Here at WeCan Insurance, we compare quotes with our numerous insurance partners to offer you
+              message2="Here at WeCan Insurance, we compare quotes with our numerous insurance partners to offer you
           personalized service with the best rates and coverage available for your vehicle. Rest easy
           knowing that all your valuable personal items & assests are insured."
-            listIntro="WeCan Insurance offers home insurance for many property types including:"
-            items={homeList}
-          />
-          <ServiceTemplate
-            title="Commercial Insurance"
-            message="Building a business means investing money, time, energy and countless resources. Without 
+              listIntro="WeCan Insurance offers home insurance for many property types including:"
+              items={homeList}
+            />
+          </div>
+          <div id="commercial">
+            <ServiceTemplate
+              title="Commercial Insurance"
+              message="Building a business means investing money, time, energy and countless resources. Without 
             commercial insurance, all those investments could dissapear overnight for any number of reasons.
             Property damage, lawsuits and business fraud are just a few reasons why businesses need
             commercial insurance."
-            message2="Here at WeCan Insurance, we compare quotes with our numerous insurance partners to ensure all
+              message2="Here at WeCan Insurance, we compare quotes with our numerous insurance partners to ensure all
             parts of your business are properly protected for the lowest rates."
-            items={[]}
-          />
-          <ServiceTemplate
-            title="Travel Insurance"
-            message="Whether you're traveling to another country for a business meeting or taking some
+              items={[]}
+            />
+          </div>
+          <div id="trave">
+            <ServiceTemplate
+              title="Travel Insurance"
+              message="Whether you're traveling to another country for a business meeting or taking some
             time off to vaction, you'll want to protect yourself against unforeseen circumstances
             that could occur before or during your time abroad."
-            message2="Here at WeCan Insurance, we compare quotes with our numerous insurance partners to offer you
+              message2="Here at WeCan Insurance, we compare quotes with our numerous insurance partners to offer you
             personalized service with the best rates and coverage available. Rest easy
             knowing that you're protected from the above risks of travelling."
-            listIntro="Some reasons why you may want to secure travel insurance include:"
-            items={travelList}
-          />
+              listIntro="Some reasons why you may want to secure travel insurance include:"
+              items={travelList}
+            />
+          </div>
         </div>
       </div>
     );
