fabddb5a9dc10860d99694d86d8a08984c490883
Ryan Karumanchery <ryankarumanchery@gmail.com>
2018-08-14 15:01:19 -0400

    Services page now made of paper
    The material-ui 'paper' surface replaced standard background

---
+++ src/Services/ServiceTemplate.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+
+const styles = theme => ({
+  center: theme.mixins.gutters({
+    paddingTop: 16,
+    paddingBottom: 16,
+    marginTop: theme.spacing.unit * 3,
+    marginBottom: theme.spacing.unit * 3,
+    maxWidth: '80%',
+    marginRight: 'auto',
+    marginLeft: 'auto',
+  }),
+  title: {
+    marginTop: 20,
+    marginBottom: 30
+  },
+  font: {
+    fontSize: 16
+  }
+});
+
+function ServiceTemplate(props) {
+  const { classes } = props;
+
+  const items = props.items;
+  const listItems = items.map(list => <li>{list}</li>);
+
+  return (
+    <div>
+      <Paper className={classes.center} elevation={6}>
+        <Typography
+          align="center"
+          variant="headline"
+          component="h3"
+          className={classes.title}
+        >
+          <strong>{props.title}</strong>
+        </Typography>
+        <Typography className={classes.font} align="left" paragraph="true">
+          {props.message}
+        </Typography>
+        <Typography className={classes.font} align="left" paragraph="true">
+          {props.message2}
+        </Typography>
+        <Typography className={classes.font}>
+          {props.listIntro}
+        </Typography>
+        <Typography className={classes.font}>
+          <ul>{listItems}</ul>
+        </Typography>
+      </Paper>
+    </div>
+  );
+}
+
+ServiceTemplate.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(ServiceTemplate);
--- src/Services/services.jsx
+++ src/Services/services.jsx
@@ -6,9 +6,33 @@ import Home from "./home";
 import Commercial from "./commercial";
 import Travel from "./travel";
 import MobileSimpleSlider from "../Mobile/MobileCarousel";
+import ServiceTemplate from "./ServiceTemplate";
 
 export default class Services extends React.Component {
   render() {
+    const autoList = [
+      "Cars",
+      "Motorcycles",
+      "ATVs",
+      "Snow Mobiles",
+      "Motorhomes",
+      "Antique / Collector Vehicles"
+    ];
+    const homeList = [
+      "Houses",
+      "Condominiums",
+      "Townhouses",
+      "Cottages",
+      "Recreational Properties",
+      "Rented Properties"
+    ];
+    const travelList = [
+      "Sickness when abroad",
+      "Injuries sustained abroad",
+      "Lost/Delayed/Stolen baggage",
+      "If travel insurance is a requirement in the country",
+      "Emergency cancellations"
+    ]
     return (
       // Make this a carousel selector
       <div>
@@ -17,10 +41,50 @@ export default class Services extends React.Component {
           <MobileSimpleSlider />
         </div>
         <div className="detailedInsurance">
-          <Auto />
-          <Home />
-          <Commercial />
-          <Travel />
+          <ServiceTemplate
+            title="Auto Insurance"
+            message="Vehicle insurance in Ontario is not just a
+                     preventative measure, it's a requirement. If you are caught driving without valid vehicle insurance,
+                    you may face fines from $5,000 - $50,000 and risk having your driver's license 
+                    suspended and vehicle impounded."
+            message2="Here at WeCan Insurance, we compare quotes with our numerous insurance partners to offer you 
+            personalized service with the best rates and coverage available for your vehicle."
+            listIntro="We offer insurance for many vehicles including:"
+            items={autoList}
+          />
+          <ServiceTemplate
+            title="Home Insurance"
+            message="A house is often the largest financial investment someone will make in their entire life,
+          so it makes sense to protect that investment from potential disasters. Water damage, theft,
+          fire, crashes from aircraft or automobiles, and explosions are just a few possible reasons
+          to consider purchasing house insurance."
+            message2="Here at WeCan Insurance, we compare quotes with our numerous insurance partners to offer you
+          personalized service with the best rates and coverage available for your vehicle. Rest easy
+          knowing that all your valuable personal items & assests are insured."
+            listIntro="WeCan Insurance offers home insurance for many property types including:"
+            items={homeList}
+          />
+          <ServiceTemplate
+            title="Commercial Insurance"
+            message="Building a business means investing money, time, energy and countless resources. Without 
+            commercial insurance, all those investments could dissapear overnight for any number of reasons.
+            Property damage, lawsuits and business fraud are just a few reasons why businesses need
+            commercial insurance."
+            message2="Here at WeCan Insurance, we compare quotes with our numerous insurance partners to ensure all
+            parts of your business are properly protected for the lowest rates."
+            items={[]}
+          />
+          <ServiceTemplate
+            title="Travel Insurance"
+            message="Whether you're traveling to another country for a business meeting or taking some
+            time off to vaction, you'll want to protect yourself against unforeseen circumstances
+            that could occur before or during your time abroad."
+            message2="Here at WeCan Insurance, we compare quotes with our numerous insurance partners to offer you
+            personalized service with the best rates and coverage available. Rest easy
+            knowing that you're protected from the above risks of travelling."
+            listIntro="Some reasons why you may want to secure travel insurance include:"
+            items={travelList}
+          />
         </div>
       </div>
     );
