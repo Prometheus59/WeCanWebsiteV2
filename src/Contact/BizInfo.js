02939db5d4945924081c4437b0671e53083dee31
Ryan Karumanchery <ryankarumanchery@gmail.com>
2018-08-14 16:15:45 -0400

    Modified contact info
    Phone number, fax added.

--- src/Contact/BizInfo.js
+++ src/Contact/BizInfo.js
@@ -1,29 +1,28 @@
-import React from 'react'
+import React from "react";
 
 export default class BizInfo extends React.Component {
-  render () {
-    var x = 'We';
-    var y = 'Can';
-    var z = 'Temp';
-    var at = '@';
-    var a = 'sound';
-    var b = 'insurance';
-    var c = '.ca';
+  render() {
+    var x = "We";
+    var y = "Can";
+    var z = "Temp";
+    var at = "@";
+    var a = "sound";
+    var b = "insurance";
+    var c = ".ca";
     return (
-      
-      <div className='contact-info'>
-        <div id='address'>
-          Fax Address
-          <br /> (000) 000 - 000
+      <div className="contact-info">
+        <div id="address">
+          Call/Text: (647) 949 - 4241
           <br /> {x + y + z + at + a + b + c}
+          <br /> Fax: (416) 756 - 1635
           <br />
         </div>
-        <div id='hours'>
+        <div id="hours">
           Hours of Operation
           <br /> Monday-Friday
           <br /> 8:30am - 4:30pm
         </div>
       </div>
-    )
+    );
   }
 }
