c317aea6fde0bba47778fbbc47c49f5b67a33072
Ryan Karumanchery <ryankarumanchery@gmail.com>
2018-08-15 15:06:29 -0400

    Made WeChat logo less fugly
    - Added border, background color
    - fixed alignment issues on mobile

--- src/Contact/Wechat.jsx
+++ src/Contact/Wechat.jsx
@@ -5,7 +5,7 @@ export default class WeChat extends React.Component {
   render() {
     return (
     <div id='wechat'>
-    <h2>WeChat QR code</h2>
+    <h2>WeChat QR Code</h2>
         <img id='wechatlogo' src={WeChatLogo} alt="WeCan Chat Logo" />
         <h3>ID: oyeswecan</h3>
     </div>
