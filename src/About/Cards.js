eddb544c77361773b3d4f2e5e3250edea4a1e825
Ryan Karumanchery <ryankarumanchery@gmail.com>
2018-08-28 12:48:20 -0400

    Card information updated

--- src/About/Cards.js
+++ src/About/Cards.js
@@ -10,25 +10,25 @@ function SimpleMediaCards(props) {
     <div className="card">
       <SimpleMediaCard
         title="Auto Coverage"
-        message="With so many variables going into an auto insurance quote, you'll need to phone our office and speak with one of our knowledgeable brokers"
+        message="With so many variables going into an auto insurance quote (including driving record, claims history and vehicle information), you'll need to contact one of our knowledgeable brokers to obtain a quote."
         image={car}
         link="/services#auto"
       />
       <SimpleMediaCard
-        title="House Coverage"
-        message="With so many variables going into an house insurance quote, you'll need to phone our office and speak with one of our knowledgeable brokers"
+        title="Home Coverage"
+        message="Home insurance premium is dependant on many factors including replacement cost, renovations, past claims and more. Therefore it is neccessary to speak with our brokers to determine a quote."
         image={house}
         link="/services#home"
       />
       <SimpleMediaCard
         title="Commercial Coverage"
-        message="With so many variables going into an commercial insurance quote, you'll need to phone our office and speak with one of our knowledgeable brokers"
+        message="It’s absolutely necessary to ensure your coverage is tailored to meet your unique business requirements wherein a customized policy may need to be assembled by one of our professional brokers."
         image={comm}
         link="/services#commercial"
       />
       <SimpleMediaCard
         title="Travel Coverage"
-        message="With so many variables going into an travel insurance quote, you'll need to phone our office and speak with one of our knowledgeable brokers"
+        message="When determining travel insurance, many factors influence the insurance premium, such as the length of the trip and the destination's location. For this reason, it is neccessary to speak with our brokers to determine a quote."
         image={travel}
         link="/services#travel"
       />
